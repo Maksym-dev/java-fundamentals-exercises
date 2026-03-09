fd751569bd8dc451d7d51a1fc8315077f3eaf3e5
Maksym-dev <magridin1995@gmail.com>
2026-03-09 14:11:44 +0200

    Complete 6-1-1 task

--- 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
+++ 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
@@ -1,25 +1,60 @@
 package com.bobocode.tdd;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import com.bobocode.tdd.exception.EmptyStackException;
+
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class LinkedStack<T> implements Stack<T> {
+
+    private Node<T> head;
+    private int size = 0;
+
+    public static <T> LinkedStack<T> of(T... elements) {
+        LinkedStack<T> linkedStack = new LinkedStack<>();
+        Stream.of(elements).forEach(linkedStack::push);
+        return linkedStack;
+    }
+
     @Override
     public void push(T element) {
-        throw new ExerciseNotCompletedException(); // todo
+        Objects.requireNonNull(element);
+        Node<T> newNode = new Node<>(element);
+        if (head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+        size++;
     }
     
     @Override
     public T pop() {
-        throw new ExerciseNotCompletedException(); // todo
+        if (head != null) {
+            size--;
+            T element = head.element;
+            this.head = head.next;
+            return element;
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException(); // todo
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo
+        return size == 0;
+    }
+
+    private static class Node<T> {
+        T element;
+        Node<T> next;
+
+        private Node(T element) {
+            this.element = element;
+        }
     }
 }
--- 6-0-test-driven-development/6-1-1-stack/src/test/java/com/bobocode/tdd/StackTest.java
+++ 6-0-test-driven-development/6-1-1-stack/src/test/java/com/bobocode/tdd/StackTest.java
@@ -1,4 +1,69 @@
 package com.bobocode.tdd;
 
+import org.junit.jupiter.api.Test;
+
+import com.bobocode.tdd.exception.EmptyStackException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StackTest {
+
+    private Stack<Integer> stack = new LinkedStack<>();
+
+    @Test
+    void testPush() {
+        stack.push(1);
+
+        assertThat(stack.pop()).isEqualTo(1);
+    }
+
+    @Test
+    void testPop() {
+        assertThrows(EmptyStackException.class, () -> stack.pop());
+    }
+
+    @Test
+    void testPushElements() {
+        stack = LinkedStack.of(2, 3, 4);
+
+        stack.push(1);
+
+        assertThat(stack.pop()).isEqualTo(1);
+    }
+
+    @Test
+    void testPopElements() {
+        stack = LinkedStack.of(5, 6, 7);
+
+        stack.pop();
+        stack.push(1);
+        int lastElement = stack.pop();
+
+        assertThat(lastElement).isEqualTo(1);
+    }
+
+    @Test
+    void testSize() {
+        stack = LinkedStack.of(5, 6, 7);
+
+        assertThat(stack.size()).isEqualTo(3);
+    }
+
+    @Test
+    void testSizeOnEmptyStack() {
+        assertThat(stack.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testIsEmpty() {
+        stack = LinkedStack.of(5, 6, 7);
+
+        assertThat(stack.isEmpty()).isFalse();
+    }
+
+    @Test
+    void testIsEmptyOnEmptyStack() {
+        assertThat(stack.isEmpty()).isTrue();
+    }
 }
